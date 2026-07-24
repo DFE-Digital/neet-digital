5b96044a8ea4c315cb88b3d91028388622bee448
Estela Gaspar <54668579+that-firefly@users.noreply.github.com>
2026-07-24 16:41:13 +0100

    Updating exams-results
    Updating-exam results

--- app/routes.js
+++ app/routes.js
@@ -316,3 +316,343 @@ router.get('/round4-mvp/What-you-can-do-next', navigationValidation(steps.what_y
     updateStepReached(req, steps.what_you_can_do_next);
     return res.render('round4-mvp/What-you-can-do-next');
 });
+
+
+
+
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////------------------ EXAM RESULTS START ------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+
+
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// What help 
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/exam-results/what-help', navigationValidation(steps.what_help), (req, res) => {
+    return res.render('/exam-results/what-help');
+});
+
+router.post('/round4-mvp/what-help', navigationValidation(steps.what_help), (req, res) => {
+
+    const errors = {};
+
+    if (!(req.session?.data?.route) || req.session.data.route.length == 0) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects what you need help with",
+            "href": "#route-1"
+        };
+    }
+
+    errors.HasErrors = Object.keys(errors).length > 0;
+
+    if (errors.HasErrors) {
+        return res.render('/round4-mvp/what-help', { errors: errors });
+    }
+
+    const valueDislikeCourse        = "I do not like my course and I’m not sure what to do when I finish" ;
+    const valuePassingResit         = "I need to pass my GCSE maths or English resit" ;
+    const valueJobsOrApprenticeship = "I'm stuck applying for jobs or apprenticeships" ; // previous Applying for jobs or get an apprenticeship
+    const valueCVorWork             = " I need help with my CV or getting work experience" ; // previous Help with my CV or get work experience
+
+    if (!req.session.data.route.includes(valueDislikeCourse)) {
+        req.session.data['course-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valuePassingResit)) {
+        req.session.data['gcse-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valueJobsOrApprenticeship)) {
+        req.session.data['jobs-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valueCVorWork)) {
+        req.session.data['cv-checkbox'] =  [];
+    }
+
+    updateStepReached(req, steps.what_help);
+    return res.redirect('/round4-mvp/what-help-dynamic');
+});
+
+// ----------------------------------------------------------------------------------------------------------------
+// What help dynamic
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/round4-mvp/what-help-dynamic', navigationValidation(steps.what_help_dynamic), (req, res) => {
+    return res.render('/round4-mvp/what-help-dynamic');
+});
+
+router.post('/round4-mvp/what-help-dynamic', navigationValidation(steps.what_help_dynamic), (req, res) => { 
+
+
+
+    const errors = {};
+    errors.HasErrors = false;
+
+     // const anySelected =
+     //   (
+     //        ((req.session.data.courseOptionsIncluded ?? false) && (req.session.data["course-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.gcseOptionsIncluded ?? false) && (req.session.data["gcse-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.jobsOptionsIncluded ?? false) && (req.session.data["jobs-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.cvOptionsIncluded ?? false) && (req.session.data["cv-checkbox"]?.length > 0 ?? false))
+    //    );
+
+
+    let d = req.session.data;
+    const routes = [].concat(d["course-checkbox"]).concat(d["gcse-checkbox"])
+                     .concat(d["jobs-checkbox"]).concat(d["cv-checkbox"])
+                     .filter(item => typeof item !== 'undefined');
+    req.session.data.SelectedRouteIds = routes;
+
+    const anySelected = routes.length > 0;
+ 
+    if (anySelected){
+        updateStepReached(req, steps.what_help_dynamic);
+        return res.redirect('/round4-mvp/where-are-you-at-with-education');
+    }
+
+    if (!errors.HasErrors && req.session.data.courseOptionsIncluded ) {
+        errors['page'] = {
+                "text": "Select at least one option that reflects what you are finding difficult",
+                "href": "#not-sure-what-do-after-college-sixth-form"
+        };
+        errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.gcseOptionsIncluded) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects what you are finding difficult",
+            "href": "#maths"
+        };
+          errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.jobsOptionsIncluded) {
+        errors['page'] = {
+             "text": "Select at least one option that reflects what you are finding difficult",
+             "href": "#do-hear-applications"
+        };
+        errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.cvOptionsIncluded) {
+        errors['page'] = {
+             "text": "Select at least one option that reflects what you are finding difficult",
+             "href": "#Create-CV-better"
+        };
+        errors.HasErrors = true;
+    }
+
+    return res.render('/round4-mvp/what-help-dynamic', { errors: errors });
+});
+
+// ----------------------------------------------------------------------------------------------------------------
+// where-are-you-at-with-education
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/round4-mvp/where-are-you-at-with-education', navigationValidation(steps.where_are_you_at_with_education), (req, res) => {
+
+
+    return res.render('/round4-mvp/where-are-you-at-with-education');
+});
+
+router.post('/round4-mvp/where-are-you-at-with-education', navigationValidation(steps.where_are_you_at_with_education), (req, res) => {
+  
+    const errors = {};
+    
+    if (!(req.session?.data?.educationStatus) || req.session.data.educationStatus.length == 0) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects where are you at with education",
+            "href": "#education-status-1"
+        };
+    }
+
+    errors.HasErrors = Object.keys(errors).length > 0;
+
+    if (errors.HasErrors) {
+        return res.render('/round4-mvp/where-are-you-at-with-education', { errors: errors });
+    }
+
+    updateStepReached(req, steps.where_are_you_at_with_education);
+    return res.redirect('/round4-mvp/check-your-answers');
+});
+
+
+
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////------------------- EXAM RESULTS END -------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+
+
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// What help 
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/exam-results/what-help', navigationValidation(steps.what_help), (req, res) => {
+    return res.render('/exam-results/what-help');
+});
+
+router.post('/exam-results/what-help', navigationValidation(steps.what_help), (req, res) => {
+
+    const errors = {};
+
+    if (!(req.session?.data?.route) || req.session.data.route.length == 0) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects what you need help with",
+            "href": "#route-1"
+        };
+    }
+
+    errors.HasErrors = Object.keys(errors).length > 0;
+
+    if (errors.HasErrors) {
+        return res.render('/exam-results/what-help', { errors: errors });
+    }
+
+    const valueDislikeCourse        = "I do not like my course and I’m not sure what to do when I finish" ;
+    const valuePassingResit         = "I need to pass my GCSE maths or English resit" ;
+    const valueJobsOrApprenticeship = "I'm stuck applying for jobs or apprenticeships" ; // previous Applying for jobs or get an apprenticeship
+    const valueCVorWork             = " I need help with my CV or getting work experience" ; // previous Help with my CV or get work experience
+
+    if (!req.session.data.route.includes(valueDislikeCourse)) {
+        req.session.data['course-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valuePassingResit)) {
+        req.session.data['gcse-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valueJobsOrApprenticeship)) {
+        req.session.data['jobs-checkbox'] =  [];
+    }
+
+    if (!req.session.data.route.includes(valueCVorWork)) {
+        req.session.data['cv-checkbox'] =  [];
+    }
+
+    updateStepReached(req, steps.what_help);
+    return res.redirect('/exam-results/what-help-dynamic');
+});
+
+// ----------------------------------------------------------------------------------------------------------------
+// What help dynamic
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/exam-results/what-help-dynamic', navigationValidation(steps.what_help_dynamic), (req, res) => {
+    return res.render('/exam-results/what-help-dynamic');
+});
+
+router.post('/exam-results/what-help-dynamic', navigationValidation(steps.what_help_dynamic), (req, res) => { 
+
+
+
+    const errors = {};
+    errors.HasErrors = false;
+
+     // const anySelected =
+     //   (
+     //        ((req.session.data.courseOptionsIncluded ?? false) && (req.session.data["course-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.gcseOptionsIncluded ?? false) && (req.session.data["gcse-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.jobsOptionsIncluded ?? false) && (req.session.data["jobs-checkbox"]?.length > 0 ?? false)) ||
+     //        ((req.session.data.cvOptionsIncluded ?? false) && (req.session.data["cv-checkbox"]?.length > 0 ?? false))
+    //    );
+
+
+    let d = req.session.data;
+    const routes = [].concat(d["course-checkbox"]).concat(d["gcse-checkbox"])
+                     .concat(d["jobs-checkbox"]).concat(d["cv-checkbox"])
+                     .filter(item => typeof item !== 'undefined');
+    req.session.data.SelectedRouteIds = routes;
+
+    const anySelected = routes.length > 0;
+ 
+    if (anySelected){
+        updateStepReached(req, steps.what_help_dynamic);
+        return res.redirect('/exam-results/where-are-you-at-with-education');
+    }
+
+    if (!errors.HasErrors && req.session.data.courseOptionsIncluded ) {
+        errors['page'] = {
+                "text": "Select at least one option that reflects what you are finding difficult",
+                "href": "#not-sure-what-do-after-college-sixth-form"
+        };
+        errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.gcseOptionsIncluded) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects what you are finding difficult",
+            "href": "#maths"
+        };
+          errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.jobsOptionsIncluded) {
+        errors['page'] = {
+             "text": "Select at least one option that reflects what you are finding difficult",
+             "href": "#do-hear-applications"
+        };
+        errors.HasErrors = true;
+    }
+
+    if (!errors.HasErrors && req.session.data.cvOptionsIncluded) {
+        errors['page'] = {
+             "text": "Select at least one option that reflects what you are finding difficult",
+             "href": "#Create-CV-better"
+        };
+        errors.HasErrors = true;
+    }
+
+    return res.render('/exam-results/what-help-dynamic', { errors: errors });
+});
+
+// ----------------------------------------------------------------------------------------------------------------
+// where-are-you-at-with-education
+// ----------------------------------------------------------------------------------------------------------------
+
+router.get('/exam-results/where-are-you-at-with-education', navigationValidation(steps.where_are_you_at_with_education), (req, res) => {
+
+
+    return res.render('/exam-results/where-are-you-at-with-education');
+});
+
+router.post('/exam-results/where-are-you-at-with-education', navigationValidation(steps.where_are_you_at_with_education), (req, res) => {
+  
+    const errors = {};
+    
+    if (!(req.session?.data?.educationStatus) || req.session.data.educationStatus.length == 0) {
+        errors['page'] = {
+            "text": "Select at least one option that reflects where are you at with education",
+            "href": "#education-status-1"
+        };
+    }
+
+    errors.HasErrors = Object.keys(errors).length > 0;
+
+    if (errors.HasErrors) {
+        return res.render('/exam-results/where-are-you-at-with-education', { errors: errors });
+    }
+
+    updateStepReached(req, steps.where_are_you_at_with_education);
+    return res.redirect('/exam-results/check-your-answers');
+});
+
+
+
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////------------------- EXAM RESULTS END -------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+/////////////////////////////////--------------------------------------------------------/////////////////////////////////
+
+
+
